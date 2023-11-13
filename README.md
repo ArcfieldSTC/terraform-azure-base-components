05f586939e9e3c411a5ddea0c2e3c31bd6b48fba
edeitrick-arcfield <149844727+edeitrick-arcfield@users.noreply.github.com>
2023-11-13 08:18:12 -0500

    feat: initial creation of module
    * docs: Added initial documentation to the README
    
    * feat: Added required terraform and provider versions
    
    * feat: Created Module Variables
    
    * feat: fixed variable typos and created main.tf
    
    * feat: created module outputs
    
    * feat: Created Terraform Test
    
    * updated workflow for this repo
    
    * updated release workflow to match repo
    
    * runner did not fire
    
    * fix: runner misconfigured
    
    * fix: still trying to get the runner to trigger
    
    * fix: adding logic for pull request to dev commit
    
    * fix: trying to fix workflows
    
    * fix: errors in the dev-commit workflow
    
    * fix: still troubleshooting runner
    
    * fix: fixing errors found in first workflow run
    
    * fix: fixing variables.tf errors
    
    * fix: adding workflow logic
    
    * fix: workflow error
    
    * fix: removing validate and adding provider base
    
    * fix: trying to fix the cache
    
    * fix: replacing TF container with direct CLI
    
    * fix: validating output of envs
    
    * fix: error OIDC TOKEN
    
    * trying to trigger a fresh token pull for the job
    
    * adding special az cli login step
    
    * fix: typo in the az cli login action
    
    * fix removing the use OIDC vars
    
    * fix: added oidc back in
    
    * fix: adding use_oidc to the test file
    
    * fix: closing provider block in test file
    
    * fix: remove ARM_OIDC from test job
    
    * fixing location variables
    
    * fix: trying with the variables in the test run
    
    * fix: trying to make some of the variables nullable
    
    * fix: swap out try for coalesce in resources

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# terraform-module-template
-Base template for creating terraform modules
+Terraform module used for instantiating base components used in Azure IaC deployments.
+This module includes the following components for deployment:
+    - Resource Group
+    - Network Security Group
+    - Virtual Network
+    - Subnets
+    - DDoS Protection Plan
+    - Azure Key Vault
---
+++ variables.tf
@@ -0,0 +1,257 @@
+#### Common variables
+variable "name-prefix" {
+  description = "value to be used as prefix for all resources"
+  type        = string
+}
+variable "primary_region" {
+  description = "primary region to deploy resources"
+  type        = string
+}
+variable "default_tags" {
+  description = "map of tags to be applied to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+### Resource group variables
+variable "create_rg" {
+  description = "create resource group"
+  type        = bool
+  default     = true
+}
+variable "rg_location" {
+  description = "location of resource group"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "rg_tags" {
+  description = "map of tags to be applied to resource group"
+  type        = map(string)
+  default     = {}
+}
+### Security group variables
+variable "create_sg" {
+  description = "create security group"
+  type        = bool
+  default     = true
+}
+variable "sg_location" {
+  description = "location of security group"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "sg_tags" {
+  description = "map of tags to be applied to security group"
+  type        = map(string)
+  default     = {}
+}
+variable "sg_rg_name" {
+  description = "resource group name for security group"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
+### Virtual network variables
+variable "create_vnet" {
+  description = "create virtual network"
+  type        = bool
+  default     = true
+}
+variable "vnet_location" {
+  description = "location of virtual network"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "vnet_rg_name" {
+  description = "resource group name for virtual network"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "vnet_cidr" {
+  description = "CIDR block for virtual network"
+  type        = string
+  validation {
+    condition     = can(cidrhost(var.vnet_cidr, 0)) && try(cidrhost(var.vnet_cidr, 0), null) == split("/", var.vnet_cidr)[0]
+    error_message = "InvalidCIDRNotation: The Address Space is not a correctly formated CIDR, or the address prefix is invalid for the CIDR's size"
+  }
+}
+variable "bgp_community" {
+  description = "BGP community for virtual network"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "vnet_ddos_id" {
+  description = "DDoS protection plan ID for virtual network"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "vnet_edge_zone" {
+  description = "Edge zone for virtual network"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "vnet_tags" {
+  description = "map of tags to be applied to virtual network"
+  type        = map(string)
+  default     = {}
+}
+
+### DDoS protection plan variables
+variable "create_ddos" {
+  description = "create DDoS protection plan"
+  type        = bool
+  default     = false
+}
+variable "ddos_location" {
+  description = "location of DDoS protection plan"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "ddos_tags" {
+  description = "map of tags to be applied to DDoS protection plan"
+  type        = map(string)
+  default     = {}
+}
+variable "ddos_rg_name" {
+  description = "resource group name for DDoS protection plan"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
+### Main Subnet variables
+variable "main_subnets_rg_name" {
+  description = "resource group name for main subnet"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "main_subnets_vnet_name" {
+  description = "virtual network name for main subnet"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "main_subnets_service_endpoints" {
+  description = "service endpoints for main subnet"
+  type        = list(string)
+  default     = []
+}
+
+### Service Dedicated Subnet variables
+variable "service_subnets_rg_name" {
+  description = "resource group name for service dedicated subnet"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "service_subnets_vnet_name" {
+  description = "virtual network name for service dedicated subnet"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "service_subnets_service_endpoints" {
+  description = "service endpoints for service dedicated subnet"
+  type        = list(string)
+  default     = []
+}
+variable "service_subnets_delegations" {
+  description = "delegations for service dedicated subnet"
+  type        = list(string)
+  default     = []
+}
+
+### Key Vault variables
+variable "create_kv" {
+  description = "create key vault"
+  type        = bool
+  default     = true
+}
+variable "kv_location" {
+  description = "location of key vault"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "kv_rg_name" {
+  description = "resource group name for key vault"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "kv_tags" {
+  description = "map of tags to be applied to key vault"
+  type        = map(string)
+  default     = {}
+}
+variable "kv_sku" {
+  description = "SKU for key vault"
+  type        = string
+  default     = "standard"
+}
+variable "kv_soft_delete_retention_days" {
+  description = "soft delete retention days for key vault"
+  type        = number
+  default     = 90
+}
+variable "kv_purge_protection_enabled" {
+  description = "purge protection enabled for key vault"
+  type        = bool
+  default     = false
+}
+variable "kv_network_acls_ip_rules" {
+  description = "network ACLs IP rules for key vault"
+  type        = list(string)
+  default     = []
+}
+variable "kv_network_acls_virtual_network_subnet_ids" {
+  description = "network ACLs virtual network subnet IDs for key vault"
+  type        = list(string)
+  default     = []
+}
+variable "kv_network_acls_bypass" {
+  description = "network ACLs bypass for key vault"
+  type        = string
+  default     = "AzureServices"
+}
+variable "kv_network_acls_default_action" {
+  description = "network ACLs default action for key vault"
+  type        = string
+  default     = "Deny"
+}
+variable "kv_tenant_id" {
+  description = "tenant ID for key vault"
+  type        = string
+  nullable    = true
+  default     = null
+}
+variable "kv_deploy_access_policy" {
+  description = "deploy access policy for key vault"
+  type        = bool
+  default     = false
+}
+variable "kv_disk_encryption_enabled" {
+  description = "disk encryption enabled for key vault"
+  type        = bool
+  default     = false
+}
+variable "kv_rbac_enabled" {
+  description = "RBAC enabled for key vault"
+  type        = bool
+  default     = false
+}
+variable "kv_template_deployment_enabled" {
+  description = "template deployment enabled for key vault"
+  type        = bool
+  default     = false
+}
